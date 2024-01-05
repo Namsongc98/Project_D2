56f3f2d9676f85384c4e7b0a1712d6fd012e77fd
unknown <namsongc98@gmail.com>
2024-01-05 18:18:41 +0700

    update

--- src/page/user/Home.tsx
+++ src/page/user/Home.tsx
@@ -1,11 +1,13 @@
 
 import Banner from "../../component/Banner";
+import ListHotel from "../../component/ListHotel";
 import SearchHotel from "../../component/SearchHotel";
 
 const Home = () => {
     return (<>
         <Banner />
         <SearchHotel/>
+        <ListHotel/>
     </>);
 }
 
